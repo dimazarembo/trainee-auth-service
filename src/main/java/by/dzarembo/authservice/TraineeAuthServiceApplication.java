4a7fbd6a4b1a81075ff1d1a1c005ed93fcc7220b
dzarembo <dima.zarembo@gmail.com>
2026-03-18 12:54:57 +0300

    Init commit

---
+++ src/main/java/by/dzarembo/authservice/TraineeAuthServiceApplication.java
@@ -0,0 +1,13 @@
+package by.dzarembo.authservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TraineeAuthServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TraineeAuthServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/by/dzarembo/authservice/TraineeAuthServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package by.dzarembo.authservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TraineeAuthServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
